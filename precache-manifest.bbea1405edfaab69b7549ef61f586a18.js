f2a8384440103f089e5365f2f8c33319e5ab6f14
Dariya Leleka <dariyaleleka@gmail.com>
2019-02-22 19:32:44 +0200

    Updates

---
+++ precache-manifest.bbea1405edfaab69b7549ef61f586a18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68bd00db83bf4933540b",
+    "url": "/todo-019/static/js/runtime~main.68bd00db.js"
+  },
+  {
+    "revision": "185ef78fb5e4c5c927fa",
+    "url": "/todo-019/static/js/main.185ef78f.chunk.js"
+  },
+  {
+    "revision": "6f7e9874499c16945f28",
+    "url": "/todo-019/static/js/2.6f7e9874.chunk.js"
+  },
+  {
+    "revision": "185ef78fb5e4c5c927fa",
+    "url": "/todo-019/static/css/main.c2a06d2b.chunk.css"
+  },
+  {
+    "revision": "71e0520be342a648ff3e6f64f45b6df6",
+    "url": "/todo-019/index.html"
+  }
+];
